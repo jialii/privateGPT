2a16d95d3450b1be6ca89643321f2af06c9d217d
jialii <jx1150@nyu.edu>
2023-07-09 21:47:56 -0400

    Update privateGPT.py

--- privateGPT.py
+++ privateGPT.py
@@ -21,7 +21,7 @@ import time
 import json
 import nltk
 import logging
-from auto_gptq import AutoGPTQForCausalLM
+# from auto_gptq import AutoGPTQForCausalLM
 from sklearn.metrics.pairwise import cosine_similarity
 from sklearn.feature_extraction.text import TfidfVectorizer
 
@@ -40,93 +40,93 @@ target_source_chunks = int(os.environ.get('TARGET_SOURCE_CHUNKS',4))
 from constants import CHROMA_SETTINGS
 
 
-def load_model(device_type, model_id, model_basename=None):
-    """
-    Select a model for text generation using the HuggingFace library.
-    If you are running this for the first time, it will download a model for you.
-    subsequent runs will use the model from the disk.
+# def load_model(device_type, model_id, model_basename=None):
+    # """
+    # Select a model for text generation using the HuggingFace library.
+    # If you are running this for the first time, it will download a model for you.
+    # subsequent runs will use the model from the disk.
 
-    Args:
-        device_type (str): Type of device to use, e.g., "cuda" for GPU or "cpu" for CPU.
-        model_id (str): Identifier of the model to load from HuggingFace's model hub.
-        model_basename (str, optional): Basename of the model if using quantized models.
-            Defaults to None.
+    # Args:
+    #     device_type (str): Type of device to use, e.g., "cuda" for GPU or "cpu" for CPU.
+    #     model_id (str): Identifier of the model to load from HuggingFace's model hub.
+    #     model_basename (str, optional): Basename of the model if using quantized models.
+    #         Defaults to None.
 
-    Returns:
-        HuggingFacePipeline: A pipeline object for text generation using the loaded model.
+    # Returns:
+    #     HuggingFacePipeline: A pipeline object for text generation using the loaded model.
 
-    Raises:
-        ValueError: If an unsupported model or device type is provided.
-    """
+    # Raises:
+    #     ValueError: If an unsupported model or device type is provided.
+    # """
 
-    logging.info(f"Loading Model: {model_id}, on: {device_type}")
-    logging.info("This action can take a few minutes!")
+    # logging.info(f"Loading Model: {model_id}, on: {device_type}")
+    # logging.info("This action can take a few minutes!")
 
-    if model_basename is not None:
-        # The code supports all huggingface models that ends with GPTQ and have some variation
-        # of .no-act.order or .safetensors in their HF repo.
-        logging.info("Using AutoGPTQForCausalLM for quantized models")
+    # if model_basename is not None:
+    #     # The code supports all huggingface models that ends with GPTQ and have some variation
+    #     # of .no-act.order or .safetensors in their HF repo.
+    #     logging.info("Using AutoGPTQForCausalLM for quantized models")
 
-        if ".safetensors" in model_basename:
-            # Remove the ".safetensors" ending if present
-            model_basename = model_basename.replace(".safetensors", "")
+    #     if ".safetensors" in model_basename:
+    #         # Remove the ".safetensors" ending if present
+    #         model_basename = model_basename.replace(".safetensors", "")
 
-        tokenizer = AutoTokenizer.from_pretrained(model_id, use_fast=True)
-        logging.info("Tokenizer loaded")
+    #     tokenizer = AutoTokenizer.from_pretrained(model_id, use_fast=True)
+    #     logging.info("Tokenizer loaded")
 
-        model = AutoGPTQForCausalLM.from_quantized(
-            model_id,
-            model_basename=model_basename,
-            use_safetensors=True,
-            trust_remote_code=True,
-            device="cuda:0",
-            use_triton=False,
-            quantize_config=None,
-        )
-    elif (
-        device_type.lower() == "cuda"
-    ):  # The code supports all huggingface models that ends with -HF or which have a .bin
-        # file in their HF repo.
-        logging.info("Using AutoModelForCausalLM for full models")
-        tokenizer = AutoTokenizer.from_pretrained(model_id)
-        logging.info("Tokenizer loaded")
+    #     model = AutoGPTQForCausalLM.from_quantized(
+    #         model_id,
+    #         model_basename=model_basename,
+    #         use_safetensors=True,
+    #         trust_remote_code=True,
+    #         device="cuda:0",
+    #         use_triton=False,
+    #         quantize_config=None,
+    #     )
+    # elif (
+    #     device_type.lower() == "cuda"
+    # ):  # The code supports all huggingface models that ends with -HF or which have a .bin
+    #     # file in their HF repo.
+    #     logging.info("Using AutoModelForCausalLM for full models")
+    #     tokenizer = AutoTokenizer.from_pretrained(model_id)
+    #     logging.info("Tokenizer loaded")
 
-        model = AutoModelForCausalLM.from_pretrained(
-            model_id,
-            device_map="auto",
-            torch_dtype=torch.float16,
-            low_cpu_mem_usage=True,
-            trust_remote_code=True,
-            # max_memory={0: "15GB"} # Uncomment this line with you encounter CUDA out of memory errors
-        )
-        model.tie_weights()
-    else:
-        logging.info("Using LlamaTokenizer")
-        tokenizer = LlamaTokenizer.from_pretrained(model_id)
-        model = LlamaForCausalLM.from_pretrained(model_id)
+    #     model = AutoModelForCausalLM.from_pretrained(
+    #         model_id,
+    #         device_map="auto",
+    #         torch_dtype=torch.float16,
+    #         low_cpu_mem_usage=True,
+    #         trust_remote_code=True,
+    #         # max_memory={0: "15GB"} # Uncomment this line with you encounter CUDA out of memory errors
+    #     )
+    #     model.tie_weights()
+    # else:
+    #     logging.info("Using LlamaTokenizer")
+    #     tokenizer = LlamaTokenizer.from_pretrained(model_id)
+    #     model = LlamaForCausalLM.from_pretrained(model_id)
 
-    # Load configuration from the model to avoid warnings
-    generation_config = GenerationConfig.from_pretrained(model_id)
-    # see here for details:
-    # https://huggingface.co/docs/transformers/
-    # main_classes/text_generation#transformers.GenerationConfig.from_pretrained.returns
+    # # Load configuration from the model to avoid warnings
+    # generation_config = GenerationConfig.from_pretrained(model_id)
+    # # see here for details:
+    # # https://huggingface.co/docs/transformers/
+    # # main_classes/text_generation#transformers.GenerationConfig.from_pretrained.returns
 
-    # Create a pipeline for text generation
-    pipe = pipeline(
-        "text-generation",
-        model=model,
-        tokenizer=tokenizer,
-        max_length=2048,
-        temperature=0,
-        top_p=0.95,
-        repetition_penalty=1.15,
-        generation_config=generation_config,
-    )
+    # # Create a pipeline for text generation
+    # pipe = pipeline(
+    #     "text-generation",
+    #     model=model,
+    #     tokenizer=tokenizer,
+    #     max_length=2048,
+    #     temperature=0,
+    #     top_p=0.95,
+    #     repetition_penalty=1.15,
+    #     generation_config=generation_config,
+    # )
 
-    local_llm = HuggingFacePipeline(pipeline=pipe)
-    logging.info("Local LLM Loaded")
+    # local_llm = HuggingFacePipeline(pipeline=pipe)
+    # logging.info("Local LLM Loaded")
 
-    return local_llm
+    # return local_llm
 
 def find_similar_questions(document, query):
     # Tokenize the document and query into sentences
